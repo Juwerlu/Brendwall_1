ac5dc3078f981502ccd167fd06b6a30fe310669d
Elena Chepraga <juwerlu@yandex.ru>
2024-09-16 23:43:01 +0300

    first commit

--- brendwall/product/views.py
+++ brendwall/product/views.py
@@ -1,14 +1,17 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView
 
 from .forms import ProductForm
 from .models import Product
 
 
-class ProductCreateView(CreateView, ListView):
+class ProductCreateView(CreateView):
     """Функция для создания продукта через форму."""
     model = Product
     form_class = ProductForm
     template_name = 'create.html'
     success_url = reverse_lazy('product:product')
-    context_object_name = 'object_list'
+
+    def get_context_data(self, **kwargs):
+        kwargs['object_list'] = Product.objects.order_by('id')
+        return super(ProductCreateView, self).get_context_data(**kwargs)
